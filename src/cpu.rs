430ca5e22fd8f5cfda9a1cc3b1d192c402b3ff15
matdexir <matthieudesir21@gmail.com>
2023-01-01 17:46:26 +0800

    added jump instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -513,6 +513,17 @@ impl CPU {
         self.update_zero_and_negative_flags(data);
     }
 
+    fn branch(&mut self, condition: bool) {
+        if condition {
+            let jump: i8 = self.mem_read(self.program_counter) as i8;
+            let jump_addr = self
+                .program_counter
+                .wrapping_add(1)
+                .wrapping_add(jump as u16);
+            self.program_counter = jump_addr;
+        }
+    }
+
     pub fn run(&mut self) {
         let ref opcodes: HashMap<u8, &'static opcodes::OpCode> = *opcodes::OPCODES_MAP;
         loop {
